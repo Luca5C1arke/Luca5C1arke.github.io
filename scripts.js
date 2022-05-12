3aa2369f0f7d9c9c5a15d7843baf46e5521c7eb8
Lucas Clarke <clarkelj@student.unimelb.edu.au>
2022-05-12 13:23:03 +1000

    made some css changes in response to feedback

--- scripts.js
+++ scripts.js
@@ -14,6 +14,27 @@ function hamburger() {
     navSub.classList.toggle("nav-active");
 }
 
+/* accordion menu */
+/* https://www.w3schools.com/howto/howto_js_accordion.asp */
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
 /* this is all a mess */
 const getData = (pageUrl) => {
     return new Promise((resolve, reject) => {
