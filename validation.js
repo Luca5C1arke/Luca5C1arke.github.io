6983a87a3bb6e4194321cfacc6aaee2abb34b5a0
Luca5C1arke <100817998+Luca5C1arke@users.noreply.github.com>
2022-05-27 14:46:19 +1000

    started working on the form validations

---
+++ validation.js
@@ -0,0 +1,26 @@
+/* performs any required form validation */
+
+/* need to do it for
+    *postcode
+    *state
+    *phone
+    *email
+    *word
+    *num
+    *abn (11 nums)
+    *date
+    *times
+*/
+
+var postcodes = document.querySelectorAll("#postcode");
+var states = document.querySelectorAll("#state");
+var phones = document.querySelectorAll("#phone");
+var emails = document.querySelectorAll("#email");
+var words = document.querySelectorAll("#word");
+var nums = document.querySelectorAll("#num");
+var abns = document.querySelectorAll("#abn");
+var dates = document.querySelectorAll("#date");
+var times = document.querySelectorAll("#times");
+
+var form = document.querySelector("form");
+
